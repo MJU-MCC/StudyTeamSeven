aaabdc2e8269f6aaab6ebd503df983fe6d417e89
jongcoding <ialleejy@gmail.com>
2024-04-04 13:58:29 +0900

    [ADD] 4주차 백준

---
+++ BOJ/이종윤/first_qeustion/단계별 문제/심화2/1037.py
@@ -0,0 +1,13 @@
+import math
+n=int(input())
+a=input().split()
+b=list(int(x) for x in a)
+what_lcm=b[0]
+if len(a)==1:
+    print(b[0]**2)
+    exit()
+for i in range(len(b)-1):
+    what_lcm=math.lcm(what_lcm,b[i+1])
+if what_lcm in b:
+    what_lcm=what_lcm*min(b)
+print(what_lcm)
