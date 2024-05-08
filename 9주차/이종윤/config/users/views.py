9c027c9eab0900ab31533574c8a9c59e090eb448
jongcoding <ialleejy@gmail.com>
2024-05-09 02:37:42 +0900

    [UPDATE] 9주차 Instagram 로그인 기능 구현 완료

--- 9주차/이종윤/config/users/views.py
+++ 9주차/이종윤/config/users/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
 from users.forms import LoginForm
 def login_view(request):
@@ -15,7 +15,7 @@ def login_view(request):
                 login(request, user)
                 return redirect("/posts/feeds/")
             else:
-                print("로그인에 실패했습니다")
+                form.add_error(None, "입력한 자격증명에 해당하는 사용자가 없습니다")
         print("form.is_vaild():", form.is_valid())
         print("form.cleaned_data:", form.cleaned_data)
         context = {"form": form}
@@ -24,7 +24,10 @@ def login_view(request):
         form =LoginForm()
         context= {"form": form}
         return render(request, "users/login.html", context)
+def logout_view(request):
+    logout(request)
 
+    return redirect("/users/login/") 
    
-    return render(request, "users/login.html", context)
+    
 # Create your views here.
