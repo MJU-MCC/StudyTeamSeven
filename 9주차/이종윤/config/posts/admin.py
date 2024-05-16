1ad4ebd8d72c235d2bb0384a2c12d5c20dd91ae4
jongcoding <ialleejy@gmail.com>
2024-05-16 14:44:23 +0900

    [UPDATE] 10주차 FEEDS 구현

--- 9주차/이종윤/config/posts/admin.py
+++ 9주차/이종윤/config/posts/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
+from posts.models import Post,PostImage,Comment
+from django.contrib.admin.widgets import AdminFileWidget
+from django.db import models
+from django.utils.safestring import mark_safe
+import admin_thumbnails
+class CommentInline(admin.TabularInline):
+    model=Comment
+    extra =1
+
+@admin_thumbnails.thumbnail("photo")
+class PostImageInline(admin.TabularInline):
+    model=PostImage
+    extra =1
+    
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display =[
+        "id",
+        "content",
+    ]
+    inlines = [
+        CommentInline,
+        PostImageInline,
+    ]
+@admin.register(PostImage)
+class PostImageAdmin(admin.ModelAdmin):
+    list_display= [
+        "id",
+        "post",
+        "photo",
+    ]
+    
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display=[
+        "id",
+        "post",
+        "content",
+    ]
+
 
 # Register your models here.
